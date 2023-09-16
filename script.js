78332f441eae81116008f81a8ada62840b55068f
TARUNKASHYAP03 <120279744+TARUNKASHYAP03@users.noreply.github.com>
2023-09-16 11:30:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const sechand = document.getElementById("sec-hand");
+const minutehand = document.getElementById("minute-hand");
+const hourhand = document.getElementById("hour-hand");
+
+function clockTick() {
+    const date = new Date();
+    const seconds = date.getSeconds() / 60;
+    const minutes = (seconds + date.getMinutes() / 60);
+    const hours = (minutes + date.getHours()) / 12;
+
+    rotateClockHand(sechand, seconds);
+    rotateClockHand(minutehand, minutes);
+    rotateClockHand(hourhand, hours);
+}
+
+function rotateClockHand(element, rotation) {
+    element.style.setProperty('--rotate', rotation * 360);
+}
+
+setClockHands();
+setInterval(clockTick, 1000);
